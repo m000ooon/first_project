3ab22e7d5ec94c0a55f19e28eb74f684c58e775e
EmilG <fingerfight62@gmail.com>
2023-12-05 00:39:46 +0500

    Досмотрел лекцию 6.1.

--- core/models.py
+++ core/models.py
@@ -6,36 +6,15 @@ User = get_user_model()
 
 
 class Student(models.Model):
-    first_name = models.CharField(
-        'Имя',
-        max_length=50,
-        blank=True,
-    )
-    last_name = models.CharField(
-        'Фамилия',
-        max_length=50,
-        blank=True
-    )
-    middle_name = models.CharField(
-        'Отчество',
-        max_length=50,
-        blank=True,
-    )
-    contact = models.CharField(
-        'Контакты',
-        max_length=255,
-        blank=True,
-    )
-    is_active = models.BooleanField(
-        'Активен',
-        default=True,
-    )
-    dc = models.DateTimeField('Создан',
-                              auto_now_add=True,
-                              )
+    first_name = models.CharField('Имя', max_length=50, blank=True)
+    last_name = models.CharField('Фамилия', max_length=50, blank=True)
+    middle_name = models.CharField('Отчество', max_length=50, blank=True)
+    contact = models.CharField('Контакты', max_length=255, blank=True)
+    is_active = models.BooleanField('Активен', default=True)
+    dc = models.DateTimeField('Создан', auto_now_add=True,)
 
     def __str__(self):
-        return self.get_full_name_str
+        return self.get_full_name_str()
 
     def get_full_name_str(self):
         first_name = self.first_name
@@ -51,48 +30,22 @@ class Student(models.Model):
 
 
 class Project(models.Model):
-    student = models.ForeignKey(
-        Student,
-        verbose_name='Студент',
-        on_delete=models.PROTECT,
-        related_name='projects',
-    )
+    student = models.ForeignKey(Student, verbose_name='Студент', on_delete=models.PROTECT, related_name='projects')
     url = models.URLField('URL')
+    dc = models.DateTimeField('Создан', auto_now_add=True)
 
     def __str__(self):
         return f'Проект студента {str(self.student)}'
 
 
 class Task(models.Model):
-    students = models.ManyToManyField(
-        Student,
-        verbose_name='студент',
-        related_name='tasks',
-    )
-    projects = models.ManyToManyField(
-        Project,
-        verbose_name='проекты',
-        related_name='tasks'
-    ),
-    title = models.CharField(
-        'Название',
-        max_length=255,
-    )
-    description = models.TextField(
-        'Описание',
-        blank=True,
-    )
-    creator = models.ForeignKey(
-        User,
-        verbose_name='Создатель задачи',
-        related_name='tasks',
-        on_delete=models.SET_NULL,
-        null=True,
-    )
-    deadline = models.DateField(
-        'Дедлайн',
-        null=True,
-    )
+    students = models.ManyToManyField(Student, verbose_name='студент', related_name='tasks')
+    projects = models.ManyToManyField(Project, verbose_name='проекты', related_name='tasks')
+    title = models.CharField('Название', max_length=255)
+    description = models.TextField('Описание', blank=True)
+    creator = models.ForeignKey(User, verbose_name='Создатель задачи', related_name='tasks', on_delete=models.SET_NULL, null=True,)
+    deadline = models.DateField('Дедлайн', null=True)
+    dc = models.DateTimeField('Создан', auto_now_add=True)
 
     def __str__(self):
         return self.title
